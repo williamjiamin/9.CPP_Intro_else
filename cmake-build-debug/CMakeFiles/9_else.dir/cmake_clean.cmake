c023c0dbf998c0674305687f88f4cd5b4a669b00
williamjiamin <lxodgf@163.com>
2020-06-26 22:38:48 +0800

    init commit

---
+++ cmake-build-debug/CMakeFiles/9_else.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "9_else"
+  "9_else.pdb"
+  "CMakeFiles/9_else.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9_else.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "-------------Auto Trading V0.01____________" << endl;
+    cout << "1. Start our Auto-Trading program" << endl;
+    cout << "2. Run Back Testing" << endl;
+    cout << "3. Run Web Scraper program" << endl;
+    cout << "4. Search off-line Database" << endl;
+    cout << "5. Quit this program " << endl;
+    cout << "-------------Auto Trading V0.01____________" << endl;
+
+    cout << "Please enter your selection (1-5): " << flush;
+
+    int value;
+    cin >> value;
+
+    if (value < 4) {
+        cout << "You must be William to use this program" << endl;
+    } else {
+        cout << "You must be root to use this option" << endl;
+    }
+
+    if (value == 5) {
+        cout << "You have quited this program" << endl;
+    } else {
+        cout << "You are still in the program" << endl;
+    }
+
+
+    return 0;
+}
